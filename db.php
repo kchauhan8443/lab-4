38d3637d5a9f3bebc39f295f168d53f91b03a31b
Kapil Chauhan <57587486+kchauhan8443@users.noreply.github.com>
2020-04-10 23:29:08 -0400

    Add files via upload

---
+++ db.php
@@ -0,0 +1,7 @@
+<?php
+
+//connect to db
+$conn = new PDO('mysql:host=xxxx;dbname=gcxxxxxxxxx', 'xxxxxxxxxxx', 'x');
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+?>
---
+++ search.php
@@ -0,0 +1,18 @@
+<html>
+<body>
+
+<?php
+
+require_once('header.php');
+
+?>
+
+<form method="get" action="search-results.php"></form>
+<div>
+	<label>Keywords</label>
+	<input name="keywords" />
+</div>
+<button>Search</button>
+
+</body>
+</html>
